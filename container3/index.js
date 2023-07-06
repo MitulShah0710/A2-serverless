395edbe20dca4507f739e9f7dee5078d69856c23
Mitul Shah <mt597534@dal.ca>
2023-07-06 11:02:47 -0300

    third container

--- container3/index.js
+++ container3/index.js
@@ -1,83 +1,101 @@
-const express = require('express');
-const Firestore = require('@google-cloud/firestore');
+const express = require("express");
+const cors = require("cors");
+const fireBaseAdmin = require("firebase-admin");
 
 const app = express();
+const port = 5000;
 
-// Firestore configuration
-const firestore = new Firestore({
-    projectId: 'edu-project-390516',
-    keyFileName: './edu-project-390516-ea8e0196f717.json'
+app.use(cors());
+app.use(express.json());
+
+const fireBaseAdminCred = require("../container3/edu-project-390516-ea8e0196f717.json");
+fireBaseAdmin.initializeApp({
+  credential: fireBaseAdmin.credential.cert(fireBaseAdminCred),
 });
 
-// Middleware to update user state to "online" in Firestore when logged in
-const updateUserState = async (req, res, next) => {
-  const sessionId = req.headers['x-session-id'];
-  if (sessionId) {
-    try {
-      // Update the user state to "online" in Firestore
-      await firestore.collection('state').doc(sessionId).set({ state: 'online' });
-    } catch (error) {
-      console.error('Error updating user state:', error);
-    }
-  }
-  next();
-};
+const db = fireBaseAdmin.firestore();
 
-// Middleware to check if the user is logged in
-const isLoggedIn = (req, res, next) => {
-  const sessionId = req.headers['x-session-id'];
-  if (!sessionId) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
-  next();
-};
+// Creating a POST API that fetches the profile of currently logged in user and all the online users
+app.post("/list", (req, res) => {
+  const { documentId } = req.body;
 
-// Endpoint to start a new session
-app.post('/login', async (req, res) => {
-  try {
-    // Code to validate the login credentials and start a new session
-    // ...
+  // Fetching the current user based on the userId received in the request body
+  db.collection("Reg")
+    .doc(documentId)
+    .get()
+    .then((docSnapshot) => {
+      if (docSnapshot.exists) {
+        const documentData = docSnapshot.data();
+        console.log(documentData);
+        db.collection("state")
+          .where("state", "==", "online")
+          .get()
+          .then((stateSnapshot) => {
+            const onlineUserIds = stateSnapshot.docs.map(
+              (doc) => doc.data().userId
+            );
 
-    // Generate a new session ID and store it in the response headers
-    const sessionId = generateSessionId();
-    res.setHeader('x-session-id', sessionId);
-    res.status(200).json({ message: 'Logged in successfully' });
-  } catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).json({ error: 'Login failed' });
-  }
+            db.collection("Reg")
+              .where(
+                fireBaseAdmin.firestore.FieldPath.documentId(),
+                "in",
+                onlineUserIds
+              )
+              .get()
+              .then((onlineUsersSnapshot) => {
+                const onlineUsers = onlineUsersSnapshot.docs.map(
+                  (doc) => doc.data().name
+                );
+                console.log(onlineUsers);
+                res.status(200).json({ documentData, onlineUsers });
+              })
+              .catch((error) => {
+                console.error("Error fetching online user names:", error);
+                res
+                  .status(500)
+                  .json({ error: "Failed to fetch online user names." });
+              });
+          })
+          .catch((error) => {
+            console.error("Error fetching online user IDs:", error);
+            res.status(500).json({ error: "Failed to fetch online user IDs." });
+          });
+      } else {
+        res.status(404).json({ error: "Document not found." });
+      }
+    })
+    .catch((error) => {
+      console.error("Error retrieving document:", error);
+      res.status(500).json({ error: "Failed to retrieve document." });
+    });
 });
 
-// Endpoint to retrieve session information
-app.get('/session', isLoggedIn, async (req, res) => {
-  const sessionId = req.headers['x-session-id'];
-  try {
-    // Code to retrieve session information from Firestore
-    // ...
+// Creating a POST API that logs the user out and updates the state in the "state" collection
+app.post("/logout", (req, res) => {
+  const { documentId } = req.body;
 
-    res.status(200).json({ sessionId, /* session data */ });
-  } catch (error) {
-    console.error('Error retrieving session information:', error);
-    res.status(500).json({ error: 'Session data not found' });
-  }
+  db.collection("state")
+    .where("userId", "==", documentId)
+    .get()
+    .then((stateSnapshot) => {
+      if (stateSnapshot.empty) {
+        res.status(404).json({ error: "User ID not found." });
+      } else {
+        const stateDocId = stateSnapshot.docs[0].id;
+        db.collection("state").doc(stateDocId).update({
+          state: "offline",
+        });
+
+        res.status(200).json({ message: "Logout successful." });
+      }
+    })
+    .catch((error) => {
+      console.error("Error logging out:", error);
+      res.status(500).json({ error: "Failed to logout." });
+    });
 });
 
-// Endpoint to end a session
-app.post('/logout', isLoggedIn, async (req, res) => {
-  const sessionId = req.headers['x-session-id'];
-  try {
-    // Code to update user state to "offline" in Firestore and delete the session
-    // ...
-
-    res.status(200).json({ message: 'Logged out successfully' });
-  } catch (error) {
-    console.error('Error during logout:', error);
-    res.status(500).json({ error: 'Logout failed' });
-  }
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
+// Creating a listener on pre-defined port
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
