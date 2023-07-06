ad2ba2333b72108eabf527783cc30bf8cae25202
Mitul Shah <mt597534@dal.ca>
2023-07-06 10:28:51 -0300

    reg success

--- container1/index.js
+++ container1/index.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Firestore = require("@google-cloud/firestore");
+const fireBaseAdmin = require("firebase-admin");
 
 const app = express();
-const firestore = new Firestore({
-    projectId: 'edu-project-390516',
-    keyFileName: './edu-project-390516-ea8e0196f717.json'
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+const fireBaseAdminCred = require("./edu-project-390516-ea8e0196f717.json");
+fireBaseAdmin.initializeApp({
+  credential: fireBaseAdmin.credential.cert(fireBaseAdminCred)
 });
 
+const db = fireBaseAdmin.firestore();
+
 app.post('/register', async (req, res) => {
-    try {
       const { name, password, email, location } = req.body;
   
       // Create a new document in the "Reg" collection with registration data
-      const registrationRef = firestore.collection('Reg').doc();
-      await registrationRef.set({
+      db.collection("Reg").add({
         name,
         password,
         email,
-        location,
-        status: 'offline'
+        location
+      }).then(() => {
+        res.status(200).json({ message: 'Registration successful' });
+      }).catch((error) => {
+        res.status(500).json({error: "Registration failed"});
       });
-  
-      res.status(200).json({ message: 'Registration successful' });
-    } catch (error) {
-      console.error('Error during registration:', error);
-      res.status(500).json({ error: 'Registration failed' });
-    }
 });
   
 
